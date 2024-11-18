8b8536ecd4456e09edfcca6453651116c7c77642
kuznikerik <erik.kuznik@gmail.com>
2024-11-18 12:43:04 +0100

    feat: add GET request

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,15 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
-// import { sql } from '@vercel/postgres';
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
 
-export function GET() {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+export async function GET() {
+  try {
+    const response = await sql`
+      SELECT * FROM mashonka_todos
+    `;
+    return NextResponse.json(response)
+  } catch (error) {
+    console.log('Error querying the database:', error)
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+  }
 }
