8491ebe1b32ec5cb0a6459c942ec7ef837a0c9cf
kuznikerik <erik.kuznik@gmail.com>
2024-11-18 11:37:44 +0100

    lint: remove unused request

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,6 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 // import { sql } from '@vercel/postgres';
 
-export function GET(request: Request) {
+export function GET() {
   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
